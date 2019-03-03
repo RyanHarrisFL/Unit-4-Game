209d7fe4062b43ebf526be3171037db030b8a3b5
Rhino <ryanharrisfl@gmail.com>
2019-03-03 16:58:46 -0500

    added JS to produce random numbers on game start

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
+   
+   
 $(document).ready(function() {
 
-// Setting my global variables to be accessed during the runtime of the application
-
+var comNumber= 0;
 var playerCounter = 0;
-var comNumber = 0;
 var crystal1 = 0;
 var crystal2 = 0;
 var crystal3 = 0;
@@ -11,12 +11,41 @@ var crystal4 = 0;
 var winner;
 var loser;
 
+
+function initializeCalculator()
+
+function getRndInteger(min, max) {
+   return Math.floor(Math.random() * (max - min + 1) ) + min;
+
+ }
+
+comNumber = getRndInteger(19, 120);
 console.log(comNumber);
-console.log("THIS SHOULD BE WORKING!!!")
+crystal1 = getRndInteger(1, 12);
+console.log(crystal1);
+crystal2 = getRndInteger(1, 12);
+console.log(crystal2);
+crystal3 = getRndInteger(1, 12);
+console.log(crystal3);
+crystal4 = getRndInteger(1, 12);
+console.log(crystal4);
+
+
+
+ function initializeCalculator() {
+   playerCounter = 10;
+   comNumber = 0;
+   crystal1 = 0;
+   crystal2 = 0;
+   crystal3 = 0;
+   crystal4 = 0;
+
+   $("#first-number, #second-number, #operator, #result").empty();
+ }
+
 
 
 });
-
 
 
 
