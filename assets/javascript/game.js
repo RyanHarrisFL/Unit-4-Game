c917b11017669fbe2151a58747c2bb609b112e45
Rhino <ryanharrisfl@gmail.com>
2019-03-03 14:39:55 -0500

    pushing up deletions and modifications to files.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+$(document).ready(function() {
+
 // Setting my global variables to be accessed during the runtime of the application
 
 var playerCounter = 0;
@@ -8,3 +10,13 @@ var crystal3 = 0;
 var crystal4 = 0;
 var winner;
 var loser;
+
+console.log(comNumber);
+console.log("THIS SHOULD BE WORKING!!!")
+
+
+});
+
+
+
+
