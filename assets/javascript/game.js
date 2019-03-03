4bca1bc6637a7060ac0d3b8208233902ff6fd7e5
Rhino <ryanharrisfl@gmail.com>
2019-03-03 13:48:55 -0500

    linked js files and created variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,10 @@
-let gameVar = 0;
+// Setting my global variables to be accessed during the runtime of the application
+
+var playerCounter = 0;
+var comNumber = 0;
+var crystal1 = 0;
+var crystal2 = 0;
+var crystal3 = 0;
+var crystal4 = 0;
+var winner;
+var loser;
