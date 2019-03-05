5a194660291f778676d8091560ea361e3396eccd
Rhino <ryanharrisfl@gmail.com>
2019-03-05 13:56:16 -0500

    fixing issues with win lose counter.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,15 @@
    
-  $(document).ready(function() {
+$(document).ready(function() {
 
-  var comNumber = Math.floor(Math.random() * (120 - 19 + 1) ) + 19;
-  var playerCounter = 0;
-  var crystal1 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
-  var crystal2 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
-  var crystal3 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
-  var crystal4 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
-  var winner = 0;
-  var loser = 0;
+var comNumber = Math.floor(Math.random() * (120 - 19 + 1) ) + 19;
+var playerCounter = 0;
+var crystal1 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
+var crystal2 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
+var crystal3 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
+var crystal4 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
+var winner = 0;
+var loser = 0;
 
-// This is the code that isn't working for the reset. :(
-
-/* var reset = function() {
-   crystal1 = 0;
-   crystal2 = 0;
-   crystal3 = 0;
-   crystal4 = 0;
-   plyaerCounter = 0;
-   comNumber = 0;
-
- };
-*/ 
-
-//$("#win-counter").text(winner);
-//$("#lose-counter").text(loser);
-
-// This generates a random numbers for the computer. 
-//function getRndInteger(min, max) {
- //  return Math.floor(Math.random() * (max - min + 1) ) + min;
-// }
 
 // Setting the values of the crystals and computer equal to the number they generate
 comNumber = Math.floor(Math.random() * (120 - 19 + 1) ) + 19;
@@ -43,12 +23,20 @@ console.log(crystal3);
 crystal4 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
 console.log(crystal4);
 
+$("#com-total").text(comNumber);
+$("#player-total").text(playerCounter);
+$("#win-counter").text(winner);
+$("#lose-counter").text(loser);
+
 var reset = function() {
   if (playerCounter === comNumber) {
      playerCounter = 0;
      winner++;
      console.log(winner);
-     $("#win-counter").text(winner);
+    $("#com-total").text(comNumber);
+    $("#player-total").text(playerCounter);
+    $("#win-counter").text(winner);
+    $("#lose-counter").text(loser);
     comNumber = Math.floor(Math.random() * (120 - 19 + 1) ) + 19;
     crystal1 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
     crystal2 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
@@ -57,6 +45,9 @@ var reset = function() {
   } else if (playerCounter > comNumber) {
     playerCounter = 0;
     loser++;
+    $("#com-total").text(comNumber);
+    $("#player-total").text(playerCounter);
+    $("#win-counter").text(winner);
     $("#lose-counter").text(loser);
     comNumber = Math.floor(Math.random() * (120 - 19 + 1) ) + 19;
     crystal1 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
@@ -66,10 +57,6 @@ var reset = function() {
     
   }
   }
-
-
-// This is displaying the computer number that was generated 
-$("#com-total").text(comNumber);
 
 $('#crystal-1').click(function() {
   playerCounter += crystal1;
