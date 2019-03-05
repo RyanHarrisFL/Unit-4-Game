0760cce9e496ac1a4b6e04116c24a1da1324f414
Rhino <ryanharrisfl@gmail.com>
2019-03-05 15:49:45 -0500

    changes to css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
    
 $(document).ready(function() {
 
+// Setting my variables to be available globally.
 var comNumber = Math.floor(Math.random() * (120 - 19 + 1) ) + 19;
 var playerCounter = 0;
 var crystal1 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
@@ -11,56 +12,46 @@ var winner = 0;
 var loser = 0;
 
 
-// Setting the values of the crystals and computer equal to the number they generate
-comNumber = Math.floor(Math.random() * (120 - 19 + 1) ) + 19;
-console.log(comNumber);
-crystal1 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
-console.log(crystal1);
-crystal2 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
-console.log(crystal2);
-crystal3 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
-console.log(crystal3);
-crystal4 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
-console.log(crystal4);
-
+// Writing the values of the computer number player total and crystals on the html.
 $("#com-total").text(comNumber);
 $("#player-total").text(playerCounter);
 $("#win-counter").text(winner);
 $("#lose-counter").text(loser);
 
+// Reset function to increment the loser or winner and reset the random numbers based on outcome of first game. 
 var reset = function() {
   if (playerCounter === comNumber) {
      playerCounter = 0;
      winner++;
      console.log(winner);
-    $("#com-total").text(comNumber);
-    $("#player-total").text(playerCounter);
-    $("#win-counter").text(winner);
-    $("#lose-counter").text(loser);
     comNumber = Math.floor(Math.random() * (120 - 19 + 1) ) + 19;
     crystal1 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
     crystal2 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
     crystal3 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
     crystal4 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
+    $("#com-total").text(comNumber);
+    $("#player-total").text(playerCounter);
+    $("#win-counter").text(winner);
+    // $("#lose-counter").text(loser);
   } else if (playerCounter > comNumber) {
     playerCounter = 0;
     loser++;
-    $("#com-total").text(comNumber);
-    $("#player-total").text(playerCounter);
-    $("#win-counter").text(winner);
-    $("#lose-counter").text(loser);
     comNumber = Math.floor(Math.random() * (120 - 19 + 1) ) + 19;
     crystal1 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
     crystal2 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
     crystal3 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
     crystal4 = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
+    $("#com-total").text(comNumber);
+    $("#player-total").text(playerCounter);
+    // $("#win-counter").text(winner);
+    $("#lose-counter").text(loser);
     
   }
   }
 
+// Buttons will add value of random number generated for each and when clicked on, update total of player counter and reset in event of win or loss
 $('#crystal-1').click(function() {
   playerCounter += crystal1;
-  console.log("test" + playerCounter);
    $("#player-total").text(playerCounter); 
    reset(); 
   
